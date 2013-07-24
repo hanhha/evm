2b2160618c83e48d7e9729b1d3fbdcf5a8a841a9
Chris Hiszpanski <thinkski@users.noreply.github.com>
2013-07-24 02:10:43 -0400

    Further work on filter

--- src/c++/main.cpp
+++ src/c++/main.cpp
@@ -1,19 +1,24 @@
 /**
  * Eulerian Video Magnification
  *
- * Copyright 2013 Vecture Labs LLC. All rights reserved.
+ * Copyright 2013 Chris Hiszpanski. All rights reserved.
  */
 
+#include <deque>
 #include <string>
+
+#include <boost/array.hpp>
+#include <boost/optional.hpp>
+#include <boost/program_options.hpp>
 
 #include <opencv2/opencv.hpp>
 
 /*!
- * Window manager. Creates and destroys window.
+ * Preview window. Creates and destroys window.
  */
-struct Window {
+struct Preview {
 	/// Constructor
-	Window(const std::string& windowName) : windowName_(windowName) {
+	Preview(const std::string& windowName) : windowName_(windowName) {
 		cv::namedWindow(windowName_);
 	}
 
@@ -23,7 +28,7 @@ struct Window {
 	}
 
 	/// Destructor
-	~Window() {
+	~Preview() {
 		cv::destroyWindow(windowName_);
 	}
 
@@ -34,15 +39,17 @@ struct Window {
 /*!
  * Filter. Temporally band-pass filters frames.
  */
-#include <array>
-#include <deque>
-#include <boost/optional.hpp>
-template<unsigned int N>
+template<typename T, std::size_t N>
 struct Filter {
+
 	/**
 	 * Constructor
+	 *
+	 * @param  fc  Center frequency
+	 * @param  bw  Bandwidth
+	 * @param  sr  Sample rate
 	 */
-	Filter() : taps_() {
+	Filter(T fc, T bw, T sr) : taps_(design(fc, bw, sr)) {
 	}
 
 	/**
@@ -65,27 +72,58 @@ struct Filter {
 		return output;
 	}
 
-	/**
-	 * TODO Make into separate func called by constructor member initializer?
-	 *
-	 * Design the filter
-	 *
-	 * Kaiser window method is used to design two low-pass filters which are
-	 * subtracted from one another to yield a band-pass filter.
-	 *
-	 * @param  w_lower  Lower freq of pass-band, as fraction of sample rate
-	 * @param  w_upper  Upper freq of pass-band, as fraction of sample rate
-	 *
-	 * @return  None
-	 */
-	void design(double w_lower, double w_upper) {
-
-	}
-
 	private:
+		/**
+		 * Design the filter
+		 *
+		 * Difference of two low-pass filtered with Blackman window is used.
+		 *
+		 * @param  fc  Center frequency
+		 * @param  bw  Bandwidth
+		 * @param  sr  Sample rate
+		 *
+		 * @return  Impulse response
+		 */
+		boost::array<T, N> design(T fc, T bw, T sr) {
+			boost::array<T, N> h;
+
+			const T f1 = (fc - bw / 2) / sr;
+			const T f2 = (fc + bw / 2) / sr;
+
+			for (std::size_t n = 0; n < N; n++) {
+				// Compute band-pass filter coefficient
+				if (n == N / 2) {
+					h[n] = 2 * (f2 - f1);
+				} else {
+					h[n] = (sin(2*M_PI * f2 * (n - N/2)) -
+						    sin(2*M_PI * f1 * (n - N/2))) /
+						   (M_PI * (n - (N/2)));
+				}
+
+				// Appy window (Blackman)
+				h[n] *= 0.42 -  0.5 * cos(2*M_PI * n / N)
+					         + 0.08 * cos(4*M_PI * n / N);
+			}
+
+			return h;
+		}
+
 		std::deque<cv::Mat> frames_;
-		std::array<double, N> taps_;
+		boost::array<T, N> taps_;
+
 };
+
+/*!
+ * Prints help information
+ */
+void help() {
+}
+
+/*!
+ * Prints version information and exists.
+ */
+void version() {
+}
 
 /*!
  * Main loop
@@ -94,29 +132,59 @@ int main(int argc, char **argv)
 {
 //	Window window("demo");
 	// TODO Parameterize filter length
-	Filter<30> filter;
 	cv::VideoCapture videoInput;
 	cv::VideoWriter videoOutput;
 
-	// Check usage
-	// TODO Replace with boost::program_options
-	if (3 != argc) {
-		std::cerr << "usage: evm <input video> <output video>" << std::endl;
-		return -1;
+	boost::program_options::options_description desc("Options");
+	desc.add_options()
+		("help", "produce help message")
+		("input-file",
+		 boost::program_options::value<std::string>(),
+		 "input video file")
+		("output-file",
+		 boost::program_options::value<std::string>(),
+		 "output video file")
+	;
+
+	boost::program_options::positional_options_description pd;
+	pd.add("input-file", 1).add("output-file", 2);
+
+	boost::program_options::variables_map vm;
+	boost::program_options::store(
+			boost::program_options::command_line_parser(argc, argv).
+				options(desc).
+				positional(pd).
+				run(),
+			vm);
+	boost::program_options::notify(vm);
+
+	if (vm.count("help")) {
+		std::cout << desc << std::endl;
+		exit(0);
+	}
+	if (!vm.count("input-file")) {
+		std::cout << "Please specify an input file" << std::endl;
+		exit(-1);
+	}
+	if (!vm.count("output-file")) {
+		std::cout << "Please specify an output file" << std::endl;
+		exit(-1);
 	}
 
 	// Open video for reading
-	videoInput.open(argv[1]);
+	videoInput.open(vm["input-file"].as<std::string>());
 	assert(videoInput.isOpened());
 
 	// Open video for writing
 	// TODO Replace hard-coded fourcc with something better
-	videoOutput.open(argv[2],
+	videoOutput.open(vm["output-file"].as<std::string>(),
 			CV_FOURCC('M', 'J', 'P', 'G'),
 			double(videoInput.get(CV_CAP_PROP_FPS)),
 			cv::Size(int(videoInput.get(CV_CAP_PROP_FRAME_WIDTH)),
 				     int(videoInput.get(CV_CAP_PROP_FRAME_HEIGHT))));
 	assert(videoOutput.isOpened());
+
+	Filter<double, 30> filter(1.5, 1., double(videoInput.get(CV_CAP_PROP_FPS)));
 
 	// Work loop
 	{
